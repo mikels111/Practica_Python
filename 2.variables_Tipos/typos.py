b4fcf0eb993960d0f0f101fdaf8d589d20d43b59
mikel <mikelseara11@gmail.com>
2020-04-23 20:46:53 +0200

    variables y tipos y abierta la carpeta operadores

---
+++ 2.variables_Tipos/typos.py
@@ -0,0 +1,21 @@
+nada =None
+print(nada)
+print(type(nada))
+
+cadena="hola me llamo mikel"
+print(type(cadena))
+
+booleano=True
+print(booleano)
+print(type(booleano))
+
+# convertir datos
+
+numero = 20
+nombre = "mikel"
+print( str(numero) + " " + nombre)
+
+# curiosidades
+nombre="mikel"
+apellido="seara"
+print("nombre" + "\n" + "apellido")
