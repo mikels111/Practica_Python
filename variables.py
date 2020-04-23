62fed9485b7a0a50d4b139a63048d690db3100c3
mikel <mikelseara11@gmail.com>
2020-04-23 19:52:17 +0200

    Primer commit

---
+++ variables.py
@@ -0,0 +1,19 @@
+#practica de python
+texto="practica"
+decimal=4.9
+print(texto)
+print(decimal)
+
+# concatenar
+
+nombre = "Mikel"
+apellido1 = "Seara"
+apellido2 = "Caballero"
+
+print(nombre+" "+apellido1+" "+apellido2)
+#concatenar con formato
+print(f"{nombre} {apellido1}")
+print("hola me llamo {} y de apellido {}".format(nombre, apellido1))
+
+# esto no es concatenación
+print(nombre,apellido1,apellido2)
