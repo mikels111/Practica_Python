42d3bf08e782d1a54b741f88e77bebb7ea0ca4e2
adminportatil <mikelseara11@gmail.com>
2020-05-06 11:39:50 +0200

    Carpeta de diccionarios y sets

---
+++ 10.Sets-Diccionarios/sets.py
@@ -0,0 +1,10 @@
+"""
+Set es un tipo de dato, para tener una colección de valores pero no tiene ni índice ni orden
+
+"""
+personas = {"Mikel", "Antonio", "Maria"}
+
+personas.add("Miguel")
+personas.remove("Antonio")
+
+print(personas  )
