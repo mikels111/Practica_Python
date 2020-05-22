e63c92bd8b03f4edebbb987e8843ca11657b9e6b
adminportatil <mikelseara11@gmail.com>
2020-05-22 18:21:34 +0200

    base de datos main

--- 19.bases_de_datos/sqlite.py
+++ 19.bases_de_datos/sqlite.py
@@ -8,9 +8,52 @@ conexion = sqlite3.connect("pruebas.db")
 cursor = conexion.cursor()
 
 # Crear tabla
-cursor.execute("CREATE TABLE IF NOT EXIST productos(id int(10) PRIMARY" +
-"KEY,AUTOINCREMENT, titulo VARCHAR(200), descripcion TEXT, precio int(255))")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS 
+productos(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+titulo VARCHAR(200),
+descripcion TEXT,
+precio int(255)
+)""")
 
+# Guardar cambios
+conexion.commit()
+
+# Insertar Datos
+cursor.execute("INSERT INTO productos VALUES(null, 'primer producto','descripcion producto', 550)")
+conexion.commit()
+
+# Borrar registros
+cursor.execute("DELETE FROM productos")
+conexion.commit()
+
+# Insertar muchos resgistros
+productos = [
+    ("Ordenador portatil", "Buen PC", 700),
+    ("Telefono Movil", "Buen telefono", 500),
+    ("Placa base", "Buen Placa", 300),
+    ("tablet", "Buen tablet", 600),
+]
+cursor.executemany("INSERT INTO productos VALUES(null, ?, ?, ?)", productos)
+conexion.commit()
+
+# Update
+cursor.execute("UPDATE productos SET precio = 525  WHERE precio = 500")
+
+# Lectura de datos
+cursor.execute("SELECT * FROM productos WHERE precio > 300")
+productos = cursor.fetchall()
+for producto in productos:
+    print("id", producto[0])
+    print("nombre del producto: ", producto[1])
+    print("Descripcion: ", producto[2])
+    print("Precio: ", producto[3])
+    print("\n")
+
+cursor.execute("SELECT descripcion FROM productos")
+producto = cursor.fetchone()
+print(producto)
 
 # Cerrar conexión
 conexion.close()
