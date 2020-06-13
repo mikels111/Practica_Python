9f86c0cf2497303318d66fb5deefa9482dce643a
adminportatil <mikelseara11@gmail.com>
2020-06-13 21:34:52 +0200

    Proyecto de terminal

--- 20.Proyecto_Python/paquete_usuarios/acciones.py
+++ 20.Proyecto_Python/paquete_usuarios/acciones.py
@@ -1,3 +1,5 @@
+import paquete_usuarios.usuario as modelo_usuario
+
 class Acciones:
     
     def registro(self):
@@ -7,6 +9,15 @@ class Acciones:
         apellidos = input("Introduce tus apellidos: ")
         email = input("Introduce tu email: ")
         password = input("Introduce una constraseña: ")
+
+        usuario = modelo_usuario.Usuario(nombre, apellidos, email, password)
+        registro = usuario.registrar()
+
+        if registro[0] >= 1:# <-- regitro [0] es el indice 0, es decir el id del usuario
+            print(f"Registro realizado, nombre: {registro[1].nombre}, email: {registro[1].email}")
+
+        else:
+            print("Usuario no registrado")
 
     def entrar(self):
         print("Has elegido entrar")
---
+++ 20.Proyecto_Python/paquete_usuarios/usuario.py
@@ -0,0 +1,33 @@
+import mysql.connector
+import datetime
+database = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "Cebanc1920",
+    database = "proyecto_python",
+    port = 3306
+)
+cursor = database.cursor(buffered=True)
+
+class Usuario:
+    
+
+    def __init__(self, nombre, apellidos, email, password):
+        self.nombre = nombre
+        self.apellidos = apellidos
+        self.email = email
+        self.password = password
+
+    def registrar(self):
+        fecha = datetime.datetime.now()
+        
+        sql = "INSERT INTO usuarios VALUES(null, %s, %s, %s, %s, %s)"
+        usuario = (self.nombre, self.apellidos, self.email, self.password, fecha)
+        cursor.execute(sql, usuario)
+        database.commit()
+
+        return [cursor.rowcount, self]
+
+
+    def identificar(self):
+        return self.nombre
