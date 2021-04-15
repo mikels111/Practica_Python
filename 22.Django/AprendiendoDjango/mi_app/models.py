f7bd0d28f9a1e827eb5884721d114af50700f6c2
mikels111 <mikelseara11@gmail.com>
2021-04-15 12:36:08 +0200

    Migraciones de modelos a base de datos

--- 22.Django/AprendiendoDjango/mi_app/models.py
+++ 22.Django/AprendiendoDjango/mi_app/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(default='null')
     public = models.BooleanField()
     create_at = models.DateTimeField(auto_now_add=True)#auto_now_add en true indica cuando se crea
     updated_at = models.DateTimeField(auto_now=True)#auto_now indica cuando se actualiza
@@ -15,3 +16,9 @@ class Category(models.Model):
     name=models.CharField(max_length=100)
     description=models.CharField(max_length=250)
     created_at=models.DateField()
+
+# Para cambiar un campo en las tablas en Models.py y que se refleje en sqlite:
+# - Hacer un cambio en models.py
+# - En la carpeta donde está manage.py: python mange.py makemigrations
+# - python manage.py sqlmigrate mi_app 0002<--numero del migrate que está en la carpeta migrations
+# -python manage.py migrate
